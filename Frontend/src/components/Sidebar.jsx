f3c8b1b734e4bda06e71c072269d267eea1c14d1
valerieehuynh <vlhuynh2@uci.edu>
2023-11-15 15:48:55 -0800

    navbar

---
+++ Frontend/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <nav className='sidebar'>
+        <div className='app-name'>
+            <p>flavrr</p>
+        </div>
+    </nav>
+  )
+}
+
+export default Sidebar
