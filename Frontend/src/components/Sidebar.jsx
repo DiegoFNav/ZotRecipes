f0d69705109b534c7dcbe7c8ad12ccb39158c878
DiegoFNav <diegonavarro12703@gmail.com>
2023-11-15 17:50:03 -0800

    Removed second John Doe

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -9,14 +9,14 @@ const Sidebar = () => {
                 <h1 class="flavrr">flavrr</h1>
             </div>
                 <div class="image-text">
-                    <span class="image">
+                    {/* <span class="image">
                         <img class="acc-pic" src="icons/profilepics.jpeg" alt="accpic"></img>
                         <p class="name">john doe</p>
                         <p class="username">@ilovegenshin</p>
-                    </span>
+                    </span> */}
                 </div>
 
-                <div class="userinfo">
+            <div class="userinfo">
                 <img class="acc-pic" src="icons/profilepics.jpeg" alt="accpic"></img>
                 <p class="name"> john doe</p>
                 <p class="username">@ilovegenshin</p>
