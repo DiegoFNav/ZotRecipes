c96cac2544b717304a36b69f60efe2b686b0a290
valerieehuynh <vlhuynh2@uci.edu>
2023-11-15 16:10:25 -0800

    fix sidebar into react

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <nav className='sidebar'>
-        <div className='app-name'>
-            <p>flavrr</p>
-        </div>
-    </nav>
+        <nav class="sidebar">
+            <div>
+                <h1 class="websitename">flavrr</h1>
+                <div class="image-text">
+                    <span class="image">
+                        <img class="acc-pic" src="icons/profilepics.jpeg" alt="accpic"></img>
+                        <p class="name">john doe</p>
+                        <p class="username">@ilovegenshin</p>
+                    </span>
+                </div>
+            </div>
+        </nav> 
   )
 }
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Sidebar from './components/Sidebar.jsx'
+import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
 )
+
+root.render(sb(Sidebar))
