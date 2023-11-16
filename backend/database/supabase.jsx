8604778116567b21c3ff035a3f0635e9fdda8d79
ALVIS YONG LAPTOP <yong.mark.alvis@gmail.com>
2023-11-15 17:55:19 -0800

    Can get random recipes and save recipes on supabase ONLY ON RECIPE TABLE

--- backend/database/supabase.jsx
+++ backend/database/supabase.jsx
@@ -1,8 +1,17 @@
 import { supabase } from '../client.js'
+import { useState, useEffect } from 'react'
+//9c4a7ac652404239b01cc2749aeafd5f
+//d29bf06c4b974e4b83a389c598283e5e
+const spoonacularApiKey = '73ee43c01e3b43a08fd690feeaca2ff9'
 
-const spoonacularApiKey = '9c4a7ac652404239b01cc2749aeafd5f'
+function getElementBeforeFirstPeriod(inputString) {
+  const index = inputString.indexOf('.');
+  if (index !== -1) {
+    return inputString.substring(0, index);
+  }
+  return inputString; // If no period is found, return the whole string
+}
 
-"<ol><li>1. Add mussels and sauce packets into boiling water. Let boil with mussels about 5 minutes to cook and deepen the broth. If using fresh mussels, wait until they open and discard closed mussels.</li><li>2. Include the noodles and cook for a 2, 3 minutes. Add the beaten egg while stirring noodles in a circular motion for even, flaky egg drops. If wanting a poached egg, carefully add the egg off to the side of the pot.</li><li>3. Meanwhile, cut scallion into thin pieces along with gim (seaweed) sheet.</li><li>4. Transfer to a serving bowl and add the scallions and gim (seaweed).</li></ol>"
 
 const getData = (url, apiKey=spoonacularApiKey) => {
   return fetch(`${url}?apiKey=${apiKey}`)
@@ -20,38 +29,62 @@ const getRandomRecipe = () => {
 }
 
 const saveRecipe = (data) => {
-  let { id, title, description, image_url, protein, calories, fat, sodium, gluten_free, dairy_free, cuisine, cooking_minutes, cheap, health_score, preparation_minutes, servings, vegan, vegetarian, extendedIngredients, instructions } = data
+  let { id, title, image, glutenFree, cuisines, readyInMinutes, veryHealthy, preparationMinutes, summary, servings, vegan, vegetarian, extendedIngredients, instructions } = data
 
+  console.log('data: ', data)
+  let description = getElementBeforeFirstPeriod(summary)
+  console.log("description", description, "summary", summary)
+  
+  let ingredientIds = [];
+  data.extendedIngredients.map((ingredient) => {
+    ingredientIds.push(ingredient.id)
+  })
 
-  console.log(data)
-  instructions = instructions.split('\n')
 
   const saveToRecipe = async () => {
     const { data, error } = await supabase
     .from('recipes')
     .insert([
-      { title: title, description: descript, image_url: image_url, protein, protein, calories: calories, fat: fat, sodium: sodium, gluten_free: gluten_free, dairy_free: dairy_free, cuisine: cuisine, cooking_minutes: cooking_minutes, cheap: cheap, health_score: health_score, preparation_minutes: preparation_minutes, servings: servings, vegan: vegan, vegetarian: vegetarian, instructions }
+      { id: id, title: title, description: description, image: image, glutenFree: glutenFree, cuisines: cuisines, readyInMinutes: readyInMinutes, veryHealthy: veryHealthy, preparationMinutes: preparationMinutes, servings: servings, vegan: vegan, vegetarian: vegetarian, instructionsIds: [parseInt(id)], ingredientIds: ingredientIds }
     ])
     .select()
-    console.log(data)
+    
+    if (error) {
+      console.log(error)
+    } else {  
+      console.log('savetorecipe runs', data)
+    }
 
   }
   saveToRecipe()
 }
-saveRecipe(getRandomRecipe())
+//saveRecipe(getRandomRecipe())
 
 
-const ReturnRecipe = async () => {
-  const recipeData = await getRandomRecipe();
-  console.log(recipeData)
+const ReturnRecipe = () => {
+  const [recipeData, setRecipeData] = useState(null)
 
-  const savedData = () => {
+
+  //uncomment if you want to see data in console
+  /*useEffect( () => {
+    const fetchData = async () => {
+      const data = await getRandomRecipe();
+      setRecipeData(data);
+    };
+
+    if (!recipeData) {
+      fetchData()
+      return
+    }
+    console.log("recipe data is : ", recipeData)
     saveRecipe(recipeData);
-  }
+
+  }, [recipeData])*/
+
 
   return (
     <>
-    <p>{recipeData && savedData()}</p>
+    <p>sup</p>
     </>
   )
 }
