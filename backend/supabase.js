02f0391777057df6efc114bd41a647ca3bbc775d
Jose <josegonz115@gmail.com>
2023-11-16 15:42:20 -0800

    organize backend

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import { supabase } from './client.js'
 
 const spoonacularApiKey = '9c4a7ac652404239b01cc2749aeafd5f'
@@ -20,20 +22,46 @@ const getRandomRecipe = () => {
 }
 
 const saveRecipe = (data) => {
-  let { id, title, description, image_url, protein, calories, fat, sodium, gluten_free, dairy_free, cuisine, cooking_minutes, cheap, health_score, preparation_minutes, servings, vegan, vegetarian, extendedIngredients, instructions } = data
+  let { id, title, image, glutenFree, cuisines, readyInMinutes, veryHealthy, preparationMinutes, summary, servings, vegan, vegetarian, extendedIngredients, instructions } = data
 
+  console.log('data: ', data)
+  let description = getElementBeforeFirstPeriod(summary)
+  console.log("description", description, "summary", summary)
+  
+  let ingredientIds = [];
+  data.extendedIngredients.map((ingredient) => {
+    ingredientIds.push(ingredient.id)
+    // TODO: Ingredient's table 
+    /* 
+    Add and run a function that puts each ingredient into ingredient's table
+    Name of Table: ingredients
+    Copy the following attributes
+    id
+    name
+    */
+  })
 
-  console.log(data)
-  instructions = instructions.split('\n')
+  // TODO: Instructions table
+  /* 
+  Name of Table: instructions
+  Copy id --> recipe_id
+  Parse Instructions as a list of strings
+  Instructions has elements in it list <li>, <p>
+  */
 
   const saveToRecipe = async () => {
     const { data, error } = await supabase
     .from('recipes')
     .insert([
-      { title: title, description: descript, image_url: image_url, protein, protein, calories: calories, fat: fat, sodium: sodium, gluten_free: gluten_free, dairy_free: dairy_free, cuisine: cuisine, cooking_minutes: cooking_minutes, cheap: cheap, health_score: health_score, preparation_minutes: preparation_minutes, servings: servings, vegan: vegan, vegetarian: vegetarian, instructions }
+      { id: id, title: title, description: description, image: image, glutenFree: glutenFree, cuisines: cuisines, readyInMinutes: readyInMinutes, veryHealthy: veryHealthy, preparationMinutes: preparationMinutes, servings: servings, vegan: vegan, vegetarian: vegetarian, instructionsIds: [parseInt(id)], ingredientIds: ingredientIds }
     ])
     .select()
-    console.log(data)
+    
+    if (error) {
+      console.log(error)
+    } else {  
+      console.log('savetorecipe runs', data)
+    }
 
   }
   saveToRecipe()
