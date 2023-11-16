f30e102449fbb1e56d6e45a4ec574f5ed4c00396
valerieehuynh <vlhuynh2@uci.edu>
2023-11-15 17:04:06 -0800

    add navbar search and explore

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -3,16 +3,47 @@ import React from 'react'
 const Sidebar = () => {
   return (
         <nav class="sidebar">
-            <div>
-                <h1 class="websitename">flavrr</h1>
-                <div class="image-text">
-                    <span class="image">
-                        <img class="acc-pic" src="icons/profilepics.jpeg" alt="accpic"></img>
-                        <p class="name">john doe</p>
-                        <p class="username">@ilovegenshin</p>
-                    </span>
-                </div>
+            <div class="namecont">
+                <h1 class="flavrr">flavrr</h1>
             </div>
+            
+            <div class="userinfo">
+                <img class="acc-pic" src="icons/profilepics.jpeg" alt="accpic"></img>
+                <p class="name"> john doe</p>
+                <p class="username">@ilovegenshin</p>
+            </div>
+
+            <div class="appsections">
+                <div class="accountcont">
+                    <img src="icons/profileacc.png"></img>
+                    <p class="account">Account</p>
+                </div>
+
+                <div class="explorecont">
+                    <img class="search"src="icons/home.png"></img>
+                    <p class="explore">Explore</p>
+                </div>
+
+                <div class="favoritescont">
+                    <img src="icons/profileacc.png"></img>
+                    <p class="favorites">Favorites</p>
+                </div>
+
+                <div class="chatcont">
+                    <img src="icons/profileacc.png"></img>
+                    <p class="chat">Chat</p>
+                </div>
+
+                <div class="settingscont">
+                    <img src="icons/profileacc.png"></img>
+                    <p class="settings">Settings</p>
+                </div>
+
+
+
+            </div>
+            
+        
         </nav> 
   )
 }
