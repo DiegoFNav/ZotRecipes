1adbe3fdd9fcc20470b6107947f1fcf61bf6ddc4
DiegoFNav <diegonavarro12703@gmail.com>
2023-11-16 15:04:42 -0800

    class changed to className in Sidebar
    could not recognize class

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-        <nav class="sidebar">
+        <nav className="sidebar">
             <div>
-            <div class="namecont">
-                <h1 class="flavrr">flavrr</h1>
+            <div className="namecont">
+                <h1 className="flavrr">flavrr</h1>
             </div>
-                <div class="image-text">
+                <div className="image-text">
                     {/* <span class="image">
                         <img class="acc-pic" src="icons/profilepics.jpeg" alt="accpic"></img>
                         <p class="name">john doe</p>
@@ -16,36 +16,36 @@ const Sidebar = () => {
                     </span> */}
                 </div>
 
-            <div class="userinfo">
-                <img class="acc-pic" src="icons/profilepics.jpeg" alt="accpic"></img>
-                <p class="name"> john doe</p>
-                <p class="username">@ilovegenshin</p>
+            <div className="userinfo">
+                <img className="acc-pic" src="icons/profilepics.jpeg" alt="accpic"></img>
+                <p className="name"> john doe</p>
+                <p className="username">@ilovegenshin</p>
             </div>
 
-            <div class="appsections">
-                <div class="accountcont">
+            <div className="appsections">
+                <div className="accountcont">
                     <img src="icons/profileacc.png"></img>
-                    <p class="account">Account</p>
+                    <p className="account">Account</p>
                 </div>
 
-                <div class="explorecont">
-                    <img class="search"src="icons/home.png"></img>
-                    <p class="explore">Explore</p>
+                <div className="explorecont">
+                    <img className="search"src="icons/home.png"></img>
+                    <p className="explore">Explore</p>
                 </div>
 
-                <div class="favoritescont">
+                <div className="favoritescont">
                     <img src="icons/profileacc.png"></img>
-                    <p class="favorites">Favorites</p>
+                    <p className="favorites">Favorites</p>
                 </div>
 
-                <div class="chatcont">
+                <div className="chatcont">
                     <img src="icons/profileacc.png"></img>
-                    <p class="chat">Chat</p>
+                    <p className="chat">Chat</p>
                 </div>
 
-                <div class="settingscont">
+                <div className="settingscont">
                     <img src="icons/profileacc.png"></img>
-                    <p class="settings">Settings</p>
+                    <p className="settings">Settings</p>
                 </div>
 
             </div>
